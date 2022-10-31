4471e71e9701a8d65814bb2e3560d79f78c7cb9f
Arthur Cordovil <devriq.cordovil@gmail.com>
2022-10-30 22:08:14 -0300

    layout fixed

--- src/App.jsx
+++ src/App.jsx
@@ -39,30 +39,29 @@ function App() {
 
 
   return (
-    <div className="bg-black text-white grid min-h-screen justify-between max-w-full">
-      
-      <nav className="border-b flex flex-col md:flex-row max-h-[10vh] px-10 h-full w-screen py-2">
-        <div className="mx-auto max-h-[4vh]">
-          <img src={LUXFLIX_LOGO} alt="" className="h-full w-full object-cover" />
+    <>      
+      <nav className="flex p-6 justify-evenly md:justify-between w-full fixed bg-neutral-900">
+        <div className="max-w-[100px] min-w-[75px] md:ml-24 lg:ml-2">
+          <img src={LUXFLIX_LOGO} alt="" className="h-full w-full object-contain" />
         </div>
 
-          <ul className="flex font-semibold text-xs mx-auto px-4 items-center justify-start h-full max-h-[5vh]  flex-1">
+          <ul className="flex md:w-1/2 items-center justify-center font-semibold text-sm mx-auto">
             <li className="px-2"><a onClick={()=>{setList(TMDB_LISTS.horror)}}>Terror</a></li>
             <li className="px-2"><a onClick={()=>{setList(TMDB_LISTS.awarded)}}>Premiados</a></li>
           </ul>
       </nav>
 
-      <main className="min-h-[80vh] grid justify-around">
+      <main className="">
 
 
-      { isLoading ? <div className="grid place-items-center">Carregando</div> : 
+      { isLoading ? <div className="grid place-items-center h-screen">Carregando</div> : 
       
-      <div className="flex px-5 flex-wrap w-full items-center justify-center">
+      <section className="flex flex-wrap w-full items-center justify-center py-16">
         
         {movies.map((movie) => {
           let movieStatusId = `movie:${movie.id}`          
           return (
-              <div key={movie.id} className={`w-[220px] movie-poster m-1 border ${status[movieStatusId]}`}>
+              <div key={movie.id} className={`w-[16rem] m-3 border-green-300 ${status[movieStatusId]}`}>
                 <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} className={`${status[movieStatusId]} h-full w-full object-cover`}/>
                 {/* <div className="movie-info">
                   <strong>{movie.title}</strong>
@@ -70,12 +69,12 @@ function App() {
               </div>
           );
         })}
-      </div>}
+      </section>}
       
       </main>
       
       <Footer/>
-    </div>
+    </>
   );
 }
 
--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,6 +1,6 @@
 function Footer() {
   return (
-    <footer className="p-4 text-center font-semibold text-sm border-t max-h-[10vh]">
+    <footer className="p-4 text-center font-semibold text-sm bg-neutral-900 fixed bottom-0 left-0 right-0">
       <p className="">
         Copyright &copy; <a href="https://github.com/devriq">DEVriq</a>
         <span id="currentYear"></span>. All rights reserved.
